e3739fb2310173f5ff062b49c84a3d7286f9a0a8
qidefei <38377305+qidefei@users.noreply.github.com>
2018-05-12 20:44:13 +0800

    Add files via upload

---
+++ StudentSystem/src/com/imooc/Student.java
@@ -0,0 +1,37 @@
+package com.imooc;
+
+public class Student {
+	 String name;
+	 String studentID;
+	 String gender;
+	 int grade;
+	  public Student(String name,String studentID,String gender) {
+		  this.name=name;
+		  this.studentID=studentID;
+		  this.gender=gender;
+	  }
+		  
+		    
+		    public String getName(){
+		        return name;
+		    }
+		    public String getgender(){
+		        return gender;
+		    }
+		    public void setGrade(int g){
+		        this.grade = g;
+		    }
+		    public String getStudentId(){
+		        return studentID;
+		    }
+                    public static void qi() {
+                    	
+                    }
+
+		}
+
+
+
+	 
+
+
